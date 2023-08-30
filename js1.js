0994d22aa1c14e1cd2d18fa6f337ffb961791b21
nhlnhlnhl <59725500+wxhnhl@users.noreply.github.com>
2023-08-30 20:30:34 +0800

    提交
    提交

---
+++ js1.js
@@ -0,0 +1,11 @@
+function writeHello(){
+	document.write("欢迎学习JS!")
+}// JavaScript Document
+function nhl(){
+	document.write("wxh")
+}
+function wxh( n){
+	console.log(typeof null);
+	
+	console.log(n);
+}
